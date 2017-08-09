8f9f894ef78bdb6cd290ea319558e063542df9f6
yushulx <lingxiao1002@gmail.com>
2017-08-09 15:11:05 +0800

    Initial commit

---
+++ popup.js
@@ -0,0 +1,5 @@
+$ = function(selector) {return document.querySelector(selector);}
+
+$('#viewer').onclick = function(e) {
+  window.open("https://www.dynamsoft.com/Demo/DWT/online_demo_scan.aspx");
+};
